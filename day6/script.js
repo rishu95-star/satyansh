72df01f3befb273cc11304f4c79f284a6b7b0250
@rishu95-star <rishupanchal95gmail.com>
2026-07-07 22:33:53 +0530

    change2

--- day6/script.js
+++ day6/script.js
@@ -1,30 +1,37 @@
 const products = [
   {
-   
+  
     name: "Laptop",
+    image: "laptop.jpeg",
     description: "High-performance laptop for work and gaming.",
     price: "₹50,000",
   },
   {
+    
     name: "Smartphone",
+    image:"mobile.jpeg",
     description: "Latest Android smartphone with amazing camera.",
-    price: "₹25,000",
+    price: "₹100,000",
   },
   {
+    
     name: "Headphones",
+    image:"headphones.jpeg",
     description: "Wireless headphones with noise cancellation.",
     price: "₹3,000",
   },
   {
     name: "Smart Watch",
+    image:"smartwatch.jpeg",
     description: "Track your health and notifications.",
     price: "₹5,000",
   },
 
   {
-    name: "Panasonic ac",
-    description: "Track your health and notifications.",
-    price: "₹5,000",
+    name: "Earphones",
+    image:"earphones.jpeg",
+    description: "High-quality earphones for music lovers.",
+    price: "₹1,500",
   },
 ];
 
@@ -36,6 +43,7 @@ cardContainer.innerHTML =
     (product) =>
       `
         <div class="card">
+         <img src="${product.image}" alt="${product.name}">
             <h2>${product.name}</h2>
             <p>${product.description}</p>
             <p class="price">${product.price}</p>
